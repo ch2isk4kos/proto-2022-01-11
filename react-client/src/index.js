f716c873ce23643eb7d73c5286bd3de7b7d6a40c
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-01 18:47:03 -0400

    connect to prismic api

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+
+import { PrismicProvider } from "@prismicio/react";
+import { client } from "./api/prismic";
+
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <PrismicProvider client={client}>
+      <App />
+    </PrismicProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
