63d415923d39eb65adcd56506de32cb529e8ce28
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-01 19:01:54 -0400

    add components for app layout, homepage and navbar

---
+++ react-client/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+// import Navbar from "./components/Navbar";
+import Navbar from "./Navbar";
+import Home from "./Home";
+
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div className="Layout">
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
+
+export default Layout;
