40c6d849704cf5a5e9e24540e51892e56aa38b09
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-05 17:17:29 -0400

    import and add react router links to nav items

--- react-client/src/components/nav/Navbar.jsx
+++ react-client/src/components/nav/Navbar.jsx
@@ -5,6 +5,8 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
+import { Link } from "react-router-dom";
+
 // const Navbar = () => {
 //   return (
 //     <div className="Navbar">
@@ -22,22 +24,43 @@ const Navigation = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="home">MSJ</Navbar.Brand>
+        <Navbar.Brand href="home">
+          <Link to="home">MSJ</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         {/* <Navbar.Collapse id="responsive-navbar-nav"> */}
         {/* <Nav className="me-auto"> */}
         <Nav className="justify-content-end">
-          <Nav.Link href="articles">Articles</Nav.Link>
+          {/* <Nav.Link href="articles">Articles</Nav.Link>
           <Nav.Link href="schedule">Schedule</Nav.Link>
           <Nav.Link href="statistic">Statistics</Nav.Link>
           <Nav.Link href="history">History</Nav.Link>
-          <Nav.Link href="store">Store</Nav.Link>
+          <Nav.Link href="store">Store</Nav.Link> */}
+          <Nav.Link>
+            <Link to="articles">Articles</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="schedule">Schedule</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="statistic">Statistics</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="history">History</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="store">Store</Link>
+          </Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link href="about">About</Nav.Link>
-          <Nav.Link href="contact">Contact</Nav.Link>
+          <Nav.Link href="about">
+            <Link>About</Link>
+          </Nav.Link>
+          <Nav.Link href="contact">
+            <Link>Contact</Link>
+          </Nav.Link>
           <Nav.Link eventKey={2} href="signin">
-            Sign In
+            <Link>Sign In</Link>
           </Nav.Link>
           {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
