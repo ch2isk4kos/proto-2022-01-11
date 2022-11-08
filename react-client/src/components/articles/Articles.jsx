47a545657be921ef874d7fdefdd82a214e131b15
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-08 16:36:34 -0500

    iterate through prismic articles and render each in a card

--- react-client/src/components/articles/Articles.jsx
+++ react-client/src/components/articles/Articles.jsx
@@ -1,21 +1,24 @@
-import React from "react";
-import {
-  PrismicRichText,
-  useFirstPrismicDocument,
-  usePrismicDocumentByUID,
-} from "@prismicio/react";
+import React, { useEffect } from "react";
+import ArticleThumbNail from "./ArticleCard";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { useAllPrismicDocumentsByType } from "@prismicio/react";
 
 const Articles = () => {
-  // const [document] = usePrismicDocumentByUID("article");
-  const [document] = useFirstPrismicDocument();
-  // const { data } = document;
+  const [articles] = useAllPrismicDocumentsByType("article");
+  console.log("articles:", articles);
   return (
     <div className="Articles">
       <h1>Articles</h1>
       <br />
-      {/* {data} */}
-      {document}
-      {/* {document && <PrismicRichText field={document.data.article} />} */}
+      <Row xs={1} md={3} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col>
+            {articles &&
+              articles.map((article) => <ArticleThumbNail article={article} />)}
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
