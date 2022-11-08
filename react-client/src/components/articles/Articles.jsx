06fd46939b4da4b277c815953ea644d972cd45a7
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-08 17:59:42 -0500

    add section containers to articles page

--- react-client/src/components/articles/Articles.jsx
+++ react-client/src/components/articles/Articles.jsx
@@ -1,24 +1,33 @@
 import React, { useEffect } from "react";
+import ArticlesHeader from "./ArticlesHeader";
 import ArticleCard from "./ArticleCard";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { useAllPrismicDocumentsByType } from "@prismicio/react";
+import { Link } from "react-router-dom";
 
 const Articles = () => {
   const [articles] = useAllPrismicDocumentsByType("article");
   console.log("articles:", articles);
   return (
-    <div className="Articles">
+    <div className="Articles" style={{ border: "2px solid blue" }}>
+      {/* HEADER */}
+      <div>
+        <ArticlesHeader />
+      </div>
+      {/* ARTICLES */}
       <h1>Articles</h1>
       <br />
-      <Row xs={1} md={3} className="g-4">
-        {Array.from({ length: 4 }).map((_, idx) => (
-          <Col>
-            {articles &&
-              articles.map((article) => <ArticleCard article={article} />)}
-          </Col>
-        ))}
-      </Row>
+      <div className="articles-grid">
+        <Row xs={1} md={3} className="g-4" style={{ border: "2px solid red" }}>
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <Col>
+              {articles &&
+                articles.map((article) => <ArticleCard article={article} />)}
+            </Col>
+          ))}
+        </Row>
+      </div>
     </div>
   );
 };
