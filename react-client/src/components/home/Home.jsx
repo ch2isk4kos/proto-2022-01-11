e68e65f301267867c8e1cc1198b43c6f95a786eb
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-10 18:06:02 -0500

    add structural changes to homepage

--- react-client/src/components/home/Home.jsx
+++ react-client/src/components/home/Home.jsx
@@ -1,63 +1,25 @@
 import React from "react";
-import Header from "./Header";
+import HomeHeader from "./HomeHeader";
 import HomeSection2 from "./HomeSection2";
 import HomeSection3 from "./HomeSection3";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Home = () => {
   return (
     <div className="Home">
-      {/* SECTION 1 */}
-      <div className="home-header">
-        <section className="left"></section>
-        <section className="middle">
-          <Header />
-          {/* <Carousel className="Carousel" variant="dark">
-            <Carousel.Item interval={2000}>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={2000}>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
-                alt="Second slide"
-              />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={2000}>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-              />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel> */}
-        </section>
-        <section className="right"></section>
-      </div>
-      <HomeSection2 />
-      <HomeSection3 />
-      {/* SECTION 2 */}
-      {/* SECTION 3 */}
+      <Container>
+        <div className="home-header">
+          <HomeHeader />
+        </div>
+        <div className="home-section-2">
+          <HomeSection2 />
+        </div>
+        <div className="home-section-3">
+          <HomeSection3 />
+        </div>
+      </Container>
     </div>
   );
 };
---
+++ react-client/src/components/home/HomeHeader.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Carousel from "react-bootstrap/Carousel";
+
+const HomeHeader = () => {
+  return (
+    <Row>
+      {/* LEFT */}
+      <Col>1 of 3</Col>
+      <Col xs={5}>
+        {/* MIDDLE */}
+        <Carousel className="Carousel" variant="dark">
+          <Carousel.Item interval={2000}>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=373940"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={2000}>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={2000}>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Col>
+      <Col>3 of 3</Col>
+      {/* RIGHT */}
+    </Row>
+  );
+};
+
+export default HomeHeader;
