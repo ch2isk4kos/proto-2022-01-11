b7130beddb09ed7dd2cc9c62fc78e51c99ff4e77
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-09 22:45:52 -0500

    add component for main application container

---
+++ react-client/src/components/MainContainer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Landing from "./Landing";
+import Home from "./home/Home";
+import About from "./About";
+import Contact from "./Contact";
+import Articles from "./articles/Articles";
+import Article from "./articles/Article";
+import Signin from "./auth/Signin";
+import { Routes, Route } from "react-router-dom";
+
+const MainContainer = () => {
+  return (
+    <main className="MainContainer">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/article/:id" element={<Article />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default MainContainer;
