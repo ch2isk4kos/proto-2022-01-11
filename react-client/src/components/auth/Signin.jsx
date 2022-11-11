2bd418dcab61c9820204614751a0d8003c5e9dd7
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-11 01:16:55 -0500

    add structural changes to signin page

--- react-client/src/components/auth/Signin.jsx
+++ react-client/src/components/auth/Signin.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import SigninForm from "./SigninForm";
+import Container from "react-bootstrap/Container";
 
 const Signin = () => {
   return (
     <div className="Signin">
       <h1>Signin</h1>
+      <Container>
+        <SigninForm />
+      </Container>
     </div>
   );
 };
